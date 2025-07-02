55097a06b4ee7efc062c4491c32eaa9898842c75
Shaine Gross <shainegross@mac.com>
2025-07-02 09:09:52 -0700

    completed Large File test in FS LAB

--- kernel/file.h
+++ kernel/file.h
@@ -26,7 +26,11 @@ struct inode {
   short minor;
   short nlink;
   uint size;
+#ifndef LAB_FS
   uint addrs[NDIRECT+1];
+#else
+  uint addrs[NDIRECT+2];
+#endif
 };
 
 // map major device number to device functions.
--- kernel/fs.h
+++ kernel/fs.h
@@ -24,9 +24,16 @@ struct superblock {
 
 #define FSMAGIC 0x10203040
 
+#ifndef LAB_FS
 #define NDIRECT 12
 #define NINDIRECT (BSIZE / sizeof(uint))
 #define MAXFILE (NDIRECT + NINDIRECT)
+#else 
+#define NDIRECT 11
+#define NINDIRECT (BSIZE / sizeof(uint))
+#define MAXFILE (NDIRECT + NINDIRECT + (NINDIRECT * NINDIRECT))
+#endif
+
 
 // On-disk inode structure
 struct dinode {
@@ -35,7 +42,11 @@ struct dinode {
   short minor;          // Minor device number (T_DEVICE only)
   short nlink;          // Number of links to inode in file system
   uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+  #ifndef LAB_FS
+  uint addrs[NDIRECT+1];
+#else
+  uint addrs[NDIRECT+2];
+#endif
 };
 
 // Inodes per block.
